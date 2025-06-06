f6ef727b098397a0bef1b2371df7c7892f7aad7d
thealepo <sanchezc.alexe@gmail.com>
2025-06-06 19:55:39 -0400

    finished ML code?

--- evaluation.py
+++ evaluation.py
@@ -1,18 +1,14 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report , confusion_matrix , accuracy_score
+from sklearn.preprocessing import LabelEncoder
 
 from ml_model.svm import svm_train_predict
 
 
 data = pd.read_csv('./data/fitness_recommendation_dataset.csv')
 df = pd.DataFrame(data)
-
-def evaluate(model , Y_test , Y_pred):
-    pass
-
 
 features = ['BFSI',
              'ABSI',
@@ -23,4 +19,31 @@ features = ['BFSI',
              'Current_Weight',
              'Ideal_Weight']
 target = 'Recommendation'
+
+# encoding any categorical features
+categorical = ['Mortality_Risk' , 'Weight_Status']
+data_encoded = data.copy()
+for col in categorical:
+    le = LabelEncoder()
+    data_encoded[col] = le.fit_transform(data[col])
+
+# encoding target
+le_target = LabelEncoder()
+data_encoded[target] = le_target.fit_transform(data[target])
+
+# train/test split
+X = data_encoded[features]
+y = data[target]
+X_train , X_test , y_train , y_test = train_test_split(
+    X , y , test_size=0.2 , stratify=y , random_state=42
+)
+
+# pred
+y_pred = svm_train_predict(X_train , y_train , X_test)
+
+def evaluate(y_true , y_pred):
+    print("Classification Report:" , classification_report(y_true,y_pred))
+    print("\nConfusion Matrix:\n" , confusion_matrix(y_true,y_pred))
+    print("\nAccuracy Score:\n" , accuracy_score(y_true,y_pred))
+evaluate(y_test , y_pred)
 
--- ml_model/svm.py
+++ ml_model/svm.py
@@ -2,37 +2,39 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import LabelEncoder
 
 def svm_train_predict(x_train , y_train , x_test):
-    # scaling data
-    scaler_X = StandardScaler()
-    scaler_Y = StandardScaler()
+    # encode target layers
+    label_encoder = LabelEncoder()
+    y_encoded = label_encoder.fit_transform(y_train)
 
-    X_train_scaled = scaler_X.fit_transform(x_train)
-    x_test_scaled = scaler_X.transform(x_test)
+    # scaling input layers
+    scaler = StandardScaler()
+    x_train_scaled = scaler.fit_transform(x_train)
+    x_test_scaled = scaler.transform(x_test)
 
-    Y_train_scaled = scaler_Y.fit_transform(y_train.values.reshape(-1,1))
-
-    # model
-    model = SVC()
-
-    # define hyperparameters
+    # define the SVM model
     param_grid = {
-        'kernel': ['linear' , 'rbf' , 'tanh'],
+        'kernel': ['linear' , 'rbf'],
         'C': [1 , 10],
         'gamma': ['scale']
     }
 
     grid_search = GridSearchCV(
-        estimator=model,
+        estimator=SVC(),
         param_grid=param_grid,
+        cv=5
     )
 
-    # model fitting
+    # fitting model
+    grid_search.fit(x_train_scaled , y_encoded)
     model = grid_search.best_estimator_
 
-    # predictions
-    predictions_scaled = model.predict(x_test_scaled)
-    predictions = scaler_Y.inverse_transform(predictions_scaled.reshape(-1,1))
+    # predict
+    predictions_encoded = model.predict(x_test_scaled)
+    predictions = label_encoder.inverse_transform(predictions)
 
     return predictions
